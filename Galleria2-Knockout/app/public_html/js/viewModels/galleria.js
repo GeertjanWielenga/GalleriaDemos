7f8542afdde2dfe5d65d0ed6cf1a1b4e261aa661
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-07-11 13:29:52 +0200

    second demo

--- Galleria2-Knockout/app/public_html/js/viewModels/galleria.js
+++ Galleria2-Knockout/app/public_html/js/viewModels/galleria.js
@@ -2,7 +2,7 @@ define(['knockout'], function (ko) {
     return function appViewModel() {
         ko.components.register("galleria", {
             viewModel: function (params) {
-                this.firstName = ko.observable('Bert');
+                this.firstName = ko.observable(params.firstName);
                 this.firstNameCaps = ko.pureComputed(function () {
                     return this.firstName().toUpperCase();
                 }, this);
